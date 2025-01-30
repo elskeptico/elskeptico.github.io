0c8edc867c8798d36ae183a611c15d771fe57795
elskeptico <128253345+elskeptico@users.noreply.github.com>
2025-01-29 16:40:09 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+    const profilePictures = ["profpic.jpg", "teslapic.png", "youtupic.png"];
+    const akaNames = ["a ghooooooost", "youuur mom!", "ElSkeptico", "is a weird acronym", "Steeeve", "AKA AKA AKA AKA" , "Your wierd hippie uncle", "Help, I'm trapped writing AKA names!", "Markus Persson :O", "Alex Hirsch"];
+
+    const profilePicture = document.getElementById("profile-picture");
+    const akaName = document.getElementById("aka-name");
+
+    profilePicture.addEventListener("click", () => {
+        const randomIndex = Math.floor(Math.random() * profilePictures.length);
+        profilePicture.src = profilePictures[randomIndex];
+    });
+
+    akaName.parentElement.addEventListener("click", () => {
+        const randomIndex = Math.floor(Math.random() * akaNames.length);
+        akaName.textContent = akaNames[randomIndex];
+    });
