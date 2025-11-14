5d23e0344fe8d355bc70f64e8d0abca213a51e8e
elskeptico <128253345+elskeptico@users.noreply.github.com>
2025-11-14 07:06:47 -0800

    I- I added things
    I- I- I added things

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-    const profilePictures = ["profpic.jpg", "teslapic.png", "youtupic.png", "eyepic.jpg"];
-    const akaNames = ["a ghooooooost", "youuur mom!", "ElSkeptico", "is a weird acronym", "Steeeve", "AKA AKA AKA AKA" , "Your wierd hippie uncle", "Help, I'm trapped writing AKA names!", "Markus Persson :O", "Alex Hirsch"];
+    const profilePictures = ["profpic.jpg", "teslapic.png", "youtupic.png", "eyepic.jpg", "badrawpic.png"];
+    const akaNames = ["a ghooooooost", "youuur mom!", "ElSkeptico", "is a weird acronym", "Steeeve", "AKA AKA AKA AKA" , "Your wierd hippie uncle", "Help, I'm trapped writing AKA names!", "Markus Persson :O", "Alex Hirsch", "NTK", "Sentient Snowglobe", "FOOD????????"];
 
     const profilePicture = document.getElementById("profile-picture");
     const akaName = document.getElementById("aka-name");
