ec7c6bcc5ebd3e21009a4a449c67cc9967539f8d
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-08-08 16:54:16 -0700

    hmm i wonder what the problem will be

---
+++ scripts/spanish.js
@@ -0,0 +1,33 @@
+const questions = ["to help", "to dance", "to drink", "to walk", "to sing", "to eat", "to buy", "to run", "ought to/should", "to draw", "to write", "to listen", "to study", "to talk/speak", "to read", "to call/be named", "to watch/look", "to ride/mount", "to swim", "to need", "to pass/spend time", "to paint", "to practice", "to prepare", "to recieve", "to review", "to touch/play an instrument", "to take in/drink", "to work", "to travel", "to live", "to skate", "to do/to make", "to have", "to want/to like", "to understand", "to think", "to be able to (can)", "to play a sport", "to go", "to be", "to be (located)", "to know"];
+const answers = ["ayudar", "bailar", "beber", "caminar", "cantar", "comer", "comprar", "correr", "deber", "dibujar", "escribir", "escuchar", "estudiar", "hablar", "leer", "llamar", "mirar", "montar", "nadar", "necesitar", "pasar", "pintar", "practicar", "preparar", "recibir", "repasar", "tocar", "tomar", "trabajar", "viajar", "vivir", "patinar", "hacer", "tener", "querer", "entender", "pensar", "poder", "juger", "ir", "ser", "estar", "saber"];
+const empty = [];
+const questionText = document.getElementById("question");
+const inputField = document.getElementById("inputfield");
+const submitButton = document.getElementById("submitbutton");
+var tempquestions = questions;
+var tempanswers = answers;
+var score = 0;
+var numberofquestions = 1;
+
+var random = Math.floor(Math.random() * questions.length);
+document.getElementById("question").textContent = questions[random];
+    
+
+submitButton.addEventListener('click', function() {
+    let answer = inputField.value;
+    if (answer.toLowerCase != answers[random]) {
+        alert("Incorrect! The correct answer was " + answers[random]);
+    } else if (answer.toLowerCase == answers[random]) {
+        alert("Correct!");
+        score += 1;
+    }
+    random = Math.floor(Math.random() * questions.length);
+    questionText.textContent = questions[random];
+    numberofquestions += 1
+});
+
+function question() {
+    var random = Math.floor(Math.random() * questions.length)
+    document.getElementById("question").textContent = questions[random];
+
+}
